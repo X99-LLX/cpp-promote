99ee35e9ba937249e6fcd261539b2d24e963ee33
wangli <wangli10@kingsoft.com>
2023-07-07 19:06:25 +0800

    feat: List

--- karray-klist/KList.h
+++ karray-klist/KList.h
@@ -1,5 +1,95 @@
 #pragma once
-class KList
+template<class T>
+struct KNode
 {
+	KNode(T data, KNode<T>* next = nullptr) : data(data), next(next) {}
+
+	T data;
+	KNode* previous;
+	KNode* next;
 };
 
+template<class T>
+class KList
+{
+private:
+	KNode<T>* head;
+	KNode<T>* tail;
+	int length;
+
+public:
+	KList() : head(nullptr), tail(nullptr), length(0) {}
+	~KList();
+
+	KNode<T>& push(T data);
+	KNode<T>& insert(KNode<T> node, int index);
+	int size();
+	void print();
+	T& operator[](int index);
+};
+
+template<class T>
+inline KList<T>::~KList()
+{
+	while (head != nullptr) {
+		KNode<T>* node = head;
+		head = head->next;
+		delete node;
+	}
+}
+
+template<class T>
+inline KNode<T>& KList<T>::push(T data)
+{
+	KNode<T>* node = new KNode<T>(data);
+	if (head == nullptr) {
+		head = tail = node;
+	}
+	else {
+		auto temp = tail;
+		node->previous = temp;
+		tail->next = node;
+		tail = node;
+	}
+	length++;
+	return *node;
+}
+
+template<class T>
+inline KNode<T>& KList<T>::insert(KNode<T> node, int index)
+{
+	
+}
+
+template<class T>
+inline int KList<T>::size()
+{
+	return this->length;
+}
+
+template<class T>
+inline void KList<T>::print()
+{
+	for(int i = 0;i < length;i++) 
+	{
+		std::cout << (*this)[i] << std::endl;
+	}
+}
+
+template<class T>
+inline T& KList<T>::operator[](int index)
+{
+	if (index >= length || index < 0)
+	{
+		throw "index is out of range!";
+	}
+
+	KNode<T>* current = head;
+	for (int i = 0; i < index; i++)
+	{
+		current = current->next;
+	}
+
+
+	return current->data;
+}
--- karray-klist/main.cpp
+++ karray-klist/main.cpp
@@ -1,30 +1,47 @@
 #include "iostream"
-#include "KArray.h"
+//#include "KArray.h"
+#include "KList.h"
+
+//void ArrayTest() 
+//{
+//	auto a = KArray<int>(1);
+//
+//	a.push(10);
+//	a.push(20);
+//	a.push(30);
+//
+//	a.insert(1, 100);
+//	a.print();
+//
+//	a.remove(0);
+//	a.print();
+//
+//	std::cout << "100 is index of " << a.findIndex(100) << std::endl;
+//	std::cout << "pop is " << a.pop() << std::endl;
+//	a.print();
+//
+//	a.clear();
+//	a.print();
+//}
+
+
+void ListTest() 
+{
+	auto a = KList<int>();
+	a.push(1);
+	a.push(2);
+	a.push(3);
+	int b = a.size();
+	std::cout << b << std::endl;
+	a.print();
+}
 
 
 int main()
 {
 	std::cout << "Hello KArray and KList" << std::endl;
 
-	auto a = KArray<int>(1);
-
-	a.push(10);
-	a.push(20);
-	a.push(30);
-
-	a.insert(1, 100);
-	a.print();
-
-	a.remove(0);
-	a.print();
-
-	std::cout << "100 is index of " << a.findIndex(100) << std::endl;
-	std::cout << "pop is " << a.pop() << std::endl;
-	a.print();
-
-	a.clear();
-	a.print();
-
+	ListTest();
 
 	return 0;
 }
